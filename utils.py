335a8a6db1d593ce6f2f6f13d90b55bb0eedcd5c
David Nemeskey <nemeskeyd@gmail.com>
2018-06-06 10:23:05 +0200

    Variable and Tensor have been merged.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from torch.autograd import Variable
 
 def repackage_hidden(h):
     """Wraps hidden states in new Variables, to detach them from their history."""
-    if type(h) == Variable:
+    if isinstance(h, Variable):
         return Variable(h.data)
     else:
         return tuple(repackage_hidden(v) for v in h)
